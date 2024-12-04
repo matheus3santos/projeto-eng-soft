14cc617f9a09008c35076e3f392ce8ab8b04ebc8
Matinhas <matheusvictor007@gmail.com>
2024-12-04 12:40:15 -0300

    .

--- backend-estagio/src/database/config.js
+++ backend-estagio/src/database/config.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 
+//ALTERAR AQUI O NOME DO BANCO, USUÁRIO E SENHA QUE VOCÊ VAI UTILIZAR
 const sequelize = new Sequelize('estagios_db', 'matheus', '12345', {
   host: 'localhost',
   dialect: 'mysql',
